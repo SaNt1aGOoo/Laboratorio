3b473ba5d20840814ef80a5068c2222d5a04652e
SaNt1aGOoo <smenarias6@gmail.com>
2026-02-04 09:52:49 -0600

    Laboratorio  03 Actividad 2

---
+++ Lab03-Act02-Santiago Mena/Lab03-Act02-Santiago Mena/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab03_Act02_Santiago_Mena
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World");
+            Console.WriteLine("Programa hecho por: Santiago Mena");
+        }
+    }
+}
