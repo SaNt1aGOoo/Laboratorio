3d0c4c54a994a5abe8c92197843ff31a95dac3b7
SaNt1aGOoo <smenarias6@gmail.com>
2026-02-11 09:48:55 -0600

    Subiendo Lab04
    Trabajamos 4 ejercicios de codigo

---
+++ Laboratorio 04/LAB04 -Act1-Santiago Mena/LAB04 -Act1-Santiago Mena/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LAB04__Act1_Santiago_Mena
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Ejercicio1
+            string modelo = "Dinoco";
+            int carga = 750;
+            float nivelcombustible = 10.5f;
+            Boolean saltoactivo = true;
+            Console.WriteLine("modelo:"+ modelo);
+            Console.WriteLine("carga:"+ carga);
+            Console.WriteLine("nivelcombustible:"+ nivelcombustible);
+            Console.WriteLine("saltoactivo:"+saltoactivo);
+
+            //Ejercicio2
+            short sensoresActivos = 128;
+            int registroProcesador = Convert.ToInt32(sensoresActivos);
+            double precisionTotal = Convert.ToDouble(sensoresActivos);
+            Console.WriteLine("precisionTotal:" + precisionTotal);
+
+            //Ejercicio3
+            double energiaGenerada = 987.65;
+            int energiaLimitada = (int)energiaGenerada;
+            Console.WriteLine("Energía Generada:"+ energiaGenerada);
+            Console.WriteLine("Energía Limitada:"+energiaLimitada);
+
+            //Ejercicio4
+            string entradaRadar;
+            Console.WriteLine("Ingrese la distnaica al planeta más cercano");
+            entradaRadar= Console.ReadLine();
+
+            int
+        }
+    }
+}
---
+++ Laboratorio 04/LAB04 -Act1-Santiago Mena/LAB04 -Act1-Santiago Mena/Properties/AssemblyInfo.cs
@@ -0,0 +1,33 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// La información general de un ensamblado se controla mediante el siguiente 
+// conjunto de atributos. Cambie estos valores de atributo para modificar la información
+// asociada a un ensamblado.
+[assembly: AssemblyTitle("LAB04 -Act1-Santiago Mena")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("LAB04 -Act1-Santiago Mena")]
+[assembly: AssemblyCopyright("Copyright ©  2026")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Si establece ComVisible en false, los tipos de este ensamblado no estarán visibles 
+// para los componentes COM.  Si es necesario obtener acceso a un tipo en este ensamblado desde 
+// COM, establezca el atributo ComVisible en true en este tipo.
+[assembly: ComVisible(false)]
+
+// El siguiente GUID sirve como id. de typelib si este proyecto se expone a COM.
+[assembly: Guid("e43bc2df-fe0b-4eff-a74b-953fb1cd8634")]
+
+// La información de versión de un ensamblado consta de los cuatro valores siguientes:
+//
+//      Versión principal
+//      Versión secundaria
+//      Número de compilación
+//      Revisión
+//
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
